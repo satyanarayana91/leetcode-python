b51201dcda16b5a7ee030da65fe562b80de6a9fe
satyanarayana91 <165493936+satyanarayana91@users.noreply.github.com>
2026-08-21 15:39:21 +0530

    Add Group Anagram solution
    Refactored the isAnagram method and implemented groupAnagrams to group anagrams from a list of strings.

--- hashmap/group_anagrams.py
+++ hashmap/group_anagrams.py
@@ -1,11 +1,11 @@
+from collections import defaultdict
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        count_s = {}
-        count_t = {}
-        for char in s:
-            count_s[char] = count_s.get(char, 0) + 1
-        for char in t:
-            count_t[char] = count_t.get(char, 0) + 1
-        return count_s == count_t
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        groups = defaultdict(list)
+        for word in strs:
+            count = [0] * 26
+            for char in word:
+                index = ord(char) - ord('a')
+                count[index]+= 1
+            groups[tuple(count)].append(word)
+        return list(groups.values())
